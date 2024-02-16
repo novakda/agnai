6ab793635d7b7ff5608d0b09a319e64b5f0cd70b
Xhiris <xhiris@gmail.com>
2024-02-15 19:55:36 -0800

    saves pngs

--- scripts/dump-chars.ts
+++ scripts/dump-chars.ts
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+import * as path from 'path';
+const png = require('png-metadata');
+
+
 import { MongoClient } from 'mongodb'
-import { exportCharacter } from '../common/characters'
+// import { exportCharacter } from '../common/characters'
 import { AppSchema } from '../common/types'
 // or as an es module:
 // import { MongoClient } from 'mongodb'
@@ -11,7 +16,22 @@ const client = new MongoClient(url)
 // Database Name
 const dbName = 'agnai'
 
+function ensureFolder(folder: fs.PathLike) {
+
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder, { recursive: true })
+  }
+
+}
+
 async function main() {
+
+  const dumpFolder = path.resolve(__dirname, 'dump')
+  const avatarBaseFolder = path.resolve('C:\\main\\aigithub\\agnai\\dist')
+  console.log(avatarBaseFolder)
+
+  ensureFolder(dumpFolder)
+
   // Use connect method to connect to the server
   await client.connect()
   console.log('Connected successfully to server')
@@ -23,27 +43,40 @@ async function main() {
   console.log('Found documents =>', findResult)
 
   findResult.map((r) => {
-    const char:AppSchema.Character = {
-        name: r.name,
-        kind: 'character',
-        userId: '',
-        createdAt: new Date().toISOString(),
-        updatedAt: new Date().toISOString(),
-        avatar: r.avatar ?? undefined,
-        _id: r._id.toString(),
-        persona: r.persona,
-        greeting: r.greeting,
-        scenario: r.scenario,
-        sampleChat: r.sampleChat
+    const char: AppSchema.Character = {
+      name: r.name,
+      kind: 'character',
+      userId: '',
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      avatar: r.avatar ?? undefined,
+      _id: r._id.toString(),
+      persona: r.persona,
+      greeting: r.greeting,
+      scenario: r.scenario,
+      sampleChat: r.sampleChat
     }
 
-    // got to cast R to character
-    const conv = exportCharacter(char, 'tavern')
-    const filetype = r.avatar ? 'png': 'json'
-    console.log(conv.name, r.avatar ?? 'no avatar', filetype)
+    if (char.avatar) {
+      const avatarFile = r.avatar ? path.join(avatarBaseFolder, r.avatar) : '(nofile)'
 
-    // decide whether to use JSON OR png
-    // based on?
+      const outFile = path.join(dumpFolder, `${r._id}.png`)
+      console.log(avatarFile, outFile)
+
+      var s = png.readFileSync(avatarFile);
+      // split
+      var list = png.splitChunk(s);
+      // append
+      var iend = list.pop(); // remove IEND
+      var newchunk = png.createChunk("aaAa", "test data");
+      list.push(newchunk);
+      list.push(iend);
+      // join
+      var newpng = png.joinChunk(list);
+      // save to file
+      fs.writeFileSync(outFile, newpng, 'binary');
+        
+    }
 
   })
 
